1bf89a582798bef30ea3919c513442e7e16b5c6c
Dennis Mellert <dennis.mellert@otto.de>
2024-11-24 22:48:35 +0100

    added script for fish

---
+++ shell/hist.fish
@@ -0,0 +1,46 @@
+function hist_reset
+    set -e hist_counter
+    commandline -r ""
+end
+
+function hist_up
+    if not set -q hist_counter
+        set -g hist_counter 0
+    else
+        set -g hist_counter (math $hist_counter + 1)
+    end
+    commandline -r (hist get --index $hist_counter)
+end
+
+function hist_down
+    if not set -q hist_counter
+        set -g hist_counter 0
+    end
+    if test $hist_counter -gt 0
+        set -g hist_counter (math $hist_counter - 1)
+    end
+    commandline -r (hist get --index $hist_counter)
+end
+
+function hist_enter
+    set current_commandline (commandline -b)
+    if test -n "$current_commandline"
+        hist record "$current_commandline"
+    end
+    set -e hist_counter
+    commandline -f execute
+end
+
+function hist_search
+    set current_commandline (commandline -b)
+    set search_result (hist search $current_commandline)
+    if test -n "$search_result"
+        commandline -r $search_result
+    end
+end
+
+bind \e\[A 'hist_up'
+bind \e\[B 'hist_down'
+bind \r 'hist_enter'
+bind \cc 'hist_reset'
+bind \cr 'hist_search'
